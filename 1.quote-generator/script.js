b3c49f17aa5988ff7436e440461150c06c090471
web223dev <ruslanus866@outlook.com>
2021-03-05 10:21:53 +0800

    Creating a loader with Javascript

--- 1.quote-generator/script.js
+++ 1.quote-generator/script.js
@@ -3,6 +3,17 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 function currentQuote(data){
     quote.textContent = data.quoteText;
@@ -14,6 +25,7 @@ function currentQuote(data){
 }
 
 async function getQuote(){
+    loading();
     try {
         const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
         const proxyUrl = 'https://fast-stream-06353.herokuapp.com/';
@@ -23,13 +35,13 @@ async function getQuote(){
     } catch (error) {
         console.log('Oops', error);
     }
+    complete();
 }
 
 function addTwitter(){
     const quote = quoteText.innerText;
     const author = authorText.innerText;
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quote}-${author}`
-    console.log(twitterUrl);
     window.open(twitterUrl, '_blank');
 }
 
